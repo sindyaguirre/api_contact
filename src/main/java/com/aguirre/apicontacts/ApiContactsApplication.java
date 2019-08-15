1039410d8cb5bcc62ffe368601d4c3938235223d
Sindy Aguirre <30392355+sindyaguirre@users.noreply.github.com>
2019-08-15 12:56:33 -0300

    Primeiro commit

---
+++ src/main/java/com/aguirre/apicontacts/ApiContactsApplication.java
@@ -0,0 +1,13 @@
+package com.aguirre.apicontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aguirre/apicontacts/ApiContactsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aguirre.apicontacts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiContactsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
